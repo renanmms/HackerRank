fcb66a6c4fc2e0ffec0c3729191b7f9073eb3d2f
Renan Martins <renanmartins1999@hotmail.com>
2022-07-24 22:38:39 -0300

    Finished Find Median

--- FindTheMedian/Program.cs
+++ FindTheMedian/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Find Median");
+var arr = new List<int>{5,3,1,2,4};
+
+Console.WriteLine(findMedian(arr));
+
+static int findMedian(List<int> arr){
+    arr.Sort();
+    var median = arr[(arr.Count() - 1)/2];
+    return median;
+}
