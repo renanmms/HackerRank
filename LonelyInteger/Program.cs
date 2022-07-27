70d7d234ca86daca8a4258607739131d830f1589
Renan Martins <renanmartins1999@hotmail.com>
2022-07-27 16:15:57 -0300

    Initializing Lonely Integer

---
+++ LonelyInteger/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+var a = new List<int>{1, 2, 3, 4, 3, 2, 1};
+
+
+static List<int> lonelyInteger(List<int> a){
+   
+    var uniqueValues = a.Distinct().ToList();
+    return uniqueValues;
+}
