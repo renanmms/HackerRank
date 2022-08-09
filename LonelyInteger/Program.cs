4c90ae72ef0db6d60b332112c3a81220d98e0291
Renan Martins <renanmartins1999@hotmail.com>
2022-08-09 14:50:54 -0300

    Finished Lonely Integer

--- LonelyInteger/Program.cs
+++ LonelyInteger/Program.cs
@@ -3,11 +3,44 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-var a = new List<int>{1, 2, 3, 4, 3, 2, 1};
+var a = new List<int>{4, 9, 95, 93, 57, 4 ,57 ,93 ,9};
+var a1 = a.Distinct();
 
+Console.WriteLine(lonelyInteger(a));
 
-static List<int> lonelyInteger(List<int> a){
-   
+static int lonelyInteger(List<int> a){
+    var aDictionary = new Dictionary<int, int>();
     var uniqueValues = a.Distinct().ToList();
-    return uniqueValues;
+    var uniqueElement = -1;
+    
+    aDictionary = initiliazeDictionary(aDictionary, uniqueValues);
+    countValues(aDictionary, a);
+    uniqueElement = findUniqueValue(aDictionary);
+
+    return uniqueElement;
+}
+
+static Dictionary<int, int> initiliazeDictionary(Dictionary<int, int> dict, List<int> uniqueValues){
+    foreach(var i in uniqueValues){
+        dict.Add(i, 0);
+    }
+    return dict;
+}
+
+static void countValues(Dictionary<int, int> dict, List<int> arr){
+    foreach(var i in arr){
+        dict[i]++;
+    }
+}
+
+static int findUniqueValue(Dictionary<int, int> dict){
+    int uniqueElement = -1;
+
+    foreach(var i in dict.Keys){
+        if(dict[i] == 1){
+            uniqueElement = i;
+        }
+    }
+
+    return uniqueElement;
 }
