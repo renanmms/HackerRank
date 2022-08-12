5f566bc19985c41fa0c0e889b5b8a55fae8f623c
Renan Martins <renanmartins1999@hotmail.com>
2022-08-12 16:05:51 -0300

    Finished Diagonal Difference

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+var matrix =  new List<List<int>>();
+var list1 = new List<int>{11, 2, 4};
+var list2 = new List<int>{4, 5, 6};
+var list3 = new List<int>{10, 8, -12};
+
+matrix.Add(list1);
+matrix.Add(list2);
+matrix.Add(list3);
+
+Console.WriteLine(diagonalDifference(matrix, 3));
+
+static int diagonalDifference(List<List<int>> arr, int n)
+    {
+        int rightToLeftDiagonal = 0;
+        int leftToRightDiagonal = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i == j){
+                    leftToRightDiagonal += arr[i][j];
+                }
+                if((i + j) == n - 1){
+                    rightToLeftDiagonal += arr[i][j];
+                }
+            }
+        }
+        return Math.Abs(rightToLeftDiagonal - leftToRightDiagonal);
+    }
