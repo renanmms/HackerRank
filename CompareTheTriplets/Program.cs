62254298ec0974bd9b12523c0bdab9c44731e643
Renan Martins <renanmartins1999@hotmail.com>
2023-05-07 20:12:01 -0300

    Adding solution with tuples

---
+++ CompareTheTriplets/Program.cs
@@ -0,0 +1,25 @@
+﻿(int clarity, int originality, int difficulty) alice = (0, 0, 0);
+(int clarity, int originality, int difficulty) bob = (0, 0, 0);;
+
+var aliceChallengesRate = Console.ReadLine()!.Split(" ");
+var bobChallengesRate = Console.ReadLine()!.Split(" ");
+
+// TODO: Get challenge rates and store in the tuples
+
+Console.WriteLine(GetComparisonPoints(alice, bob));
+
+
+(int, int) GetComparisonPoints((int clarity, int originality, int difficulty) a1,
+                        (int clarity, int originality, int difficulty) b1){    
+    var comparisonPoints = (0, 0);
+
+    comparisonPoints.Item1 += a1.clarity > b1.clarity ? 1 : 0;
+    comparisonPoints.Item1 += a1.originality > b1.originality ? 1 : 0;
+    comparisonPoints.Item1 += a1.difficulty > b1.difficulty ? 1 : 0;
+
+    comparisonPoints.Item2 += b1.clarity > a1.clarity ? 1 : 0;
+    comparisonPoints.Item2 += b1.originality > a1.originality ? 1 : 0;
+    comparisonPoints.Item2 += b1.difficulty > a1.difficulty ? 1 : 0;
+
+    return comparisonPoints;
+}
