1f08440a6f820087e230b570208420e438504fdf
Renan Martins <renanmartins1999@hotmail.com>
2023-05-07 20:34:13 -0300

    Initializing compare the triplets with arrays

---
+++ CompareTheTriplets/Program.cs
@@ -0,0 +1,11 @@
+﻿var alicePoints = new int[]{0, 0, 0};
+var bobPoints = new int[]{0, 0, 0};
+
+
+int[] GetComparisonPoints(int[] a1, int[] b1){
+    
+    var aliceTotalPoints = 0; 
+    var bobTotalPoints = 0;
+
+    return new int[]{aliceTotalPoints, bobTotalPoints};
+}
