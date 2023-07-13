ebb1589a2dcec19f59a16ff73494c7fe51c8f67b
Renan Martins <renanmartins1999@hotmail.com>
2023-07-13 05:51:33 -0300

    Adding A Very Big Sum solution

---
+++ AVeryBigSum/Program.cs
@@ -0,0 +1,9 @@
+﻿int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+List<long> ar = Console.ReadLine()?.TrimEnd().Split(' ')?.ToList().Select(arTemp => Convert.ToInt64(arTemp)).ToList()!;
+long result = aVeryBigSum(ar);
+
+Console.WriteLine(result);
+
+static long aVeryBigSum(List<long> arr){
+    return arr.Sum();
+}
