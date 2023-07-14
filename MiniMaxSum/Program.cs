637571baf11e843bae89ec74ff9a9f13fee9351c
Renan Martins <renanmartins1999@hotmail.com>
2023-07-14 06:45:55 -0300

    Adding Mini-Max Sum

---
+++ MiniMaxSum/Program.cs
@@ -0,0 +1,10 @@
+﻿var arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
+miniMaxSum(arr);
+
+void miniMaxSum(List<long> arr)
+{
+    var maximumSum = arr.Where(n => n != arr.Min()).Sum();
+    var minimumSum = arr.Where(n => n != arr.Max()).Sum();
+
+    Console.Write("{0} {1}", minimumSum, maximumSum);
+}
