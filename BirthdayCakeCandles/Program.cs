a6fc9c9298f2a820b329e0b1cce4d61e03a5a3f9
Renan Martins <renanmartins1999@hotmail.com>
2023-07-18 07:51:13 -0300

    Adding Birthday Cake Candles

---
+++ BirthdayCakeCandles/Program.cs
@@ -0,0 +1,9 @@
+﻿var candlesHeight = new List<int>(){4, 4, 1, 3};
+
+Console.WriteLine(birthdayCakeCandles(candlesHeight));
+
+int birthdayCakeCandles(List<int> candlesHeight){
+    var tallestCandle = candlesHeight.Max();
+    
+    return candlesHeight.Where(c => c == tallestCandle).Count();
+}
