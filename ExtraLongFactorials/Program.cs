5e1e8dbda30c6cf646efb9468cbc9531cc9ffc1e
Renan Martins <renanmartins1999@hotmail.com>
2023-07-28 07:54:25 -0300

    Adding ExtraLongFactorials problem

---
+++ ExtraLongFactorials/Program.cs
@@ -0,0 +1,12 @@
+﻿long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+Console.WriteLine(extraLongFactorials(n));
+
+long extraLongFactorials(long n)
+{
+    // Base case
+    if(n == 1) return n;
+
+    // Recursive case
+    return n*extraLongFactorials(n - 1);
+}
