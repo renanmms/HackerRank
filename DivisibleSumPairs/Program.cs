a3f80d0a7cbd6afd5610940938878c53872629e2
Renan Martins <renanmartins1999@hotmail.com>
2023-08-08 20:36:02 -0300

    Finished Divisible Sum Pairs challenge

---
+++ DivisibleSumPairs/Program.cs
@@ -0,0 +1,14 @@
+﻿var ar = new int[]{1, 2, 3, 4, 5, 6};
+var k = 5;
+
+var pairs = 0;
+
+for(int i = 0; i < ar.Length; i++) {
+    for(int j = 0; j < ar.Length; j++){
+        if(i < j && (ar[i] + ar[j]) % k == 0){
+            pairs++;
+        }
+    }
+}
+
+Console.WriteLine(pairs);
