a80a3df8bc68e52ed2b91ea34a1fd6b2ac8d46da
Renan Martins <renanmartins1999@hotmail.com>
2023-08-22 20:28:12 -0300

    Adding Angry Professor challenge

---
+++ AngryProfessor/Program.cs
@@ -0,0 +1,11 @@
+﻿var numberTestCases = 1;
+var cancellationThreshold = 3;
+var arrivalTimes = new int[]{-2, 1, 0, 1, 2};
+var attendees = 0;
+
+foreach(var time in arrivalTimes)
+{
+    if(time <= 0) attendees++;
+}
+
+Console.WriteLine(attendees);
