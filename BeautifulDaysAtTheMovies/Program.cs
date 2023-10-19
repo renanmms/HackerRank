ed1b02fdd660cbfe1a696e019cc0db563f29af77
Renan Martins <renanmartins1999@hotmail.com>
2023-10-18 21:42:38 -0300

    Mocking input data in beautiful days at the movies

--- BeautifulDaysAtTheMovies/Program.cs
+++ BeautifulDaysAtTheMovies/Program.cs
@@ -1,4 +1,12 @@
-﻿var givenNumber = Console.ReadLine();
-var reversedNumber = string.Join("", givenNumber.Reverse());
+﻿int k = 6;
 
-Console.ReadLine();
+for(int i = 20; i <= 23; i++) 
+{
+    var givenNumber = i;
+    var reversedNumber = Convert.ToInt32(string.Join("", i.ToString().Reverse()));
+
+    if((givenNumber - reversedNumber) % k == 0)
+    {
+        Console.WriteLine($"{i} is a beautiful day");
+    }
+}
