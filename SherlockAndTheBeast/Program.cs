9488b81c517995d0459c6e38cd01ac18260a19f1
Renan Martins <renanmartins1999@hotmail.com>
2023-11-13 21:47:54 -0300

    Initializing Sherlock and the beast challenge

---
+++ SherlockAndTheBeast/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string key = "33355555"; 
+
+bool isDecentNumber = IsDecentNumber(key);
+
+bool IsDecentNumber(string key)
+{
+	if(key.Contains('5') && key.Contains('3'))
+	{
+		Console.WriteLine($"{key} is a decent number");
+		Console.WriteLine($"Number of occurences of the number 3: {CountDigitOccurences(key, '3')}");
+		Console.WriteLine($"Number of occurences of the number 5: {CountDigitOccurences(key, '5')}");
+		return true;
+	}
+	return false;
+}
+
+int CountDigitOccurences(string key, char digit)
+{
+	int occurences = key.Where(c => c == digit).Count();
+
+	return occurences;
+}
