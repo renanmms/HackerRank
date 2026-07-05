86e48becd30ea61b6e0c0cbf44442b37b09cb58e
Renan Martins <renanmartins1999@hotmail.com>
2026-07-05 12:08:40 -0300

    solving mini max sum

--- MiniMaxSum/Program.cs
+++ MiniMaxSum/Program.cs
@@ -1,10 +1,42 @@
-﻿var arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
+﻿var arr = (Console.ReadLine() ?? string.Empty)
+    .TrimEnd()
+    .Split(' ')
+        .ToList()
+    .Select(arrTemp => Convert.ToInt64(arrTemp))
+        .ToList();
+
 miniMaxSum(arr);
 
 void miniMaxSum(List<long> arr)
 {
-    var maximumSum = arr.Where(n => n != arr.Min()).Sum();
-    var minimumSum = arr.Where(n => n != arr.Max()).Sum();
+    var sorted = arr.Order();
+    var miniSum = sorted.Take(4).Sum();
+    var maxSum = sorted.TakeLast(4).Sum();
 
-    Console.Write("{0} {1}", minimumSum, maximumSum);
+    Console.Write("{0} {1}", miniSum, maxSum);
 }
+
+// Me overcomplicating things XD
+
+// void miniMaxSum(List<long> arr)
+// {
+//     var minimumValue = arr.Min();
+//     var maximumValue = arr.Max();
+
+//     var isNormalized = minimumValue == maximumValue;
+
+//     var numberOfMinValues = arr.Where(n => n == minimumValue);
+//     var numberOfMaxValues = arr.Where(n => n == maximumValue);
+
+//     if(isNormalized)
+//     {
+//         var miniMaxSum = minimumValue + maximumValue;
+//         Console.WriteLine("{0} {1}", miniMaxSum, miniMaxSum);
+//         return;
+//     }
+
+//     var maxSum = arr.Where(n => n != minimumValue).Sum();
+//     var miniSum = arr.Where(n => n != maximumValue).Sum();
+
+//     Console.Write("{0} {1}", miniSum, maxSum);
+// }
