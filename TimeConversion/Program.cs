6917f9db0b209c762e2c2c5aa99587c4f8ed0798
Renan Martins <renanmartins1999@hotmail.com>
2026-07-08 19:52:46 -0300

    add time conversion

--- TimeConversion/Program.cs
+++ TimeConversion/Program.cs
@@ -1,13 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string date = Console.ReadLine();
-
+string date = Console.ReadLine() ?? string.Empty;
 var convertedTime = timeConversion(date);
 
-Console.WriteLine(convertedTime);
-
 static string timeConversion(string date){
-    var correctDateFormat = $"{date.Substring(0, date.Length - 2)} {date.Substring(8)}";
+    var timeOfTheDay = date.Substring(8);
+    var hour = date.Substring(0, date.Length - 2);
+    var correctDateFormat = $"{hour} {timeOfTheDay}";
+
     var parsedDate = DateTime.Parse(correctDateFormat);
-    return parsedDate.ToString().Split()[1];
+
+    return parsedDate.ToString("HH:mm:ss");
 }
