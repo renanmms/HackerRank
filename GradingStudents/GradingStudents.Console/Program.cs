5687dc2d0e563adbd65591eda4203c9bdbe10448
Renan Martins <renanmartins1999@hotmail.com>
2026-07-10 16:40:37 -0300

    adding grading students solution

---
+++ GradingStudents/GradingStudents.Console/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+
+int gradesCount = Convert.ToInt32((Console.ReadLine() ?? string.Empty).Trim());
+
+List<int> grades = [];
+
+for (int i = 0; i < gradesCount; i++)
+{
+    int gradesItem = Convert.ToInt32((Console.ReadLine() ?? string.Empty).Trim());
+    grades.Add(gradesItem);
+}
+
+List<int> result = Result.gradingStudents(grades);
+
+Console.WriteLine(string.Join("\n", result));
+
+
+public class Result
+{
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<int> gradingStudents(List<int> grades)
+    {
+        var roundedGrades = new List<int>(grades.Count);
+
+        return roundedGrades.ConvertAll(RoundGrade);
+    }
+
+    public static int RoundGrade(int grade)
+    {
+        if(grade < 38)
+        {
+            return grade;
+        }
+
+        int nextMultipleOfFive = grade + (5 - (grade % 5));
+        int difference = nextMultipleOfFive - grade;
+
+        if(difference < 3)
+        {
+            return nextMultipleOfFive;
+        }
+
+        return grade;
+    }
+}
---
+++ GradingStudents/GradingStudents.Tests/ResultTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+
+namespace GradingStudentsTests;
+
+public class ResultTests
+{
+    
+    [Theory]
+    [InlineData(29)]
+    [InlineData(33)]
+    [InlineData(37)]
+    [InlineData(0)]
+    public void Grade_IsFailingGradeLessThan38_DoNotRound(int grade)
+    {
+        var result = Result.RoundGrade(grade);
+
+        Assert.Equal(grade, result);
+    }
+
+    [Theory]
+    [InlineData(39, 40)]
+    [InlineData(84, 85)]
+    public void Grade_IsGradeGreaterThan38_DoRound(int grade, int expected)
+    {        
+        var result = Result.RoundGrade(grade);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData(57)]
+    [InlineData(42)]
+    [InlineData(76)]
+    [InlineData(96)]
+    public void Grade_DifferenceBetweenTheGradeAndTheNextMultipleOfFive_DoNotRound(int grade)
+    {
+        var result = Result.RoundGrade(grade);
+
+        Assert.Equal(grade, result);
+    }
+
+}
